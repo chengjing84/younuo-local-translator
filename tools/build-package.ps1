34502d21c7e0ff7c059f7ec22583d31416e335ec
ANNO_Yoo <zeng981009@gmail.com>
2026-09-10 17:22:21 +0800

    Redesign UI as a lightweight tool workspace

--- tools/build-package.ps1
+++ tools/build-package.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = "Stop"
 $ProjectRoot = Split-Path -Parent $PSScriptRoot
-$Version = "0.5.0"
+$Version = "0.6.0"
 $DistRoot = Join-Path $ProjectRoot "dist"
 $StageRoot = Join-Path $env:TEMP "younuo-package-$Version"
 $PackageRoot = Join-Path $StageRoot "younuo-local-translator"
